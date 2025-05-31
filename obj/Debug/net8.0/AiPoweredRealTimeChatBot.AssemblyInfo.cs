c52dade40beb6262d74f9251bebbbcad24b62def
irteja500@gmail.com <irteja500@gmail.com>
2025-05-31 23:26:47 +0600

    Added ss for readme.md file

--- obj/Debug/net8.0/AiPoweredRealTimeChatBot.AssemblyInfo.cs
+++ obj/Debug/net8.0/AiPoweredRealTimeChatBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AiPoweredRealTimeChatBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+960bd03239b57973f1f2a27fc252ad1b5877875e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AiPoweredRealTimeChatBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AiPoweredRealTimeChatBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
